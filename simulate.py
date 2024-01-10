42f87035ef7d595c41e7ac6443df4018ddc0091b
GarrettWeil <garrettweil2024@u.northwestern.edu>
2024-01-10 15:51:06 -0600

    finished google doc instructions

--- simulate.py
+++ simulate.py
@@ -1,1 +1,12 @@
-pass
+import dm_control.mujoco
+import mujoco.viewer
+import time
+
+m = dm_control.mujoco.MjModel.from_xml_path("example.xml")
+d = dm_control.mujoco.MjData(m)
+with mujoco.viewer.launch_passive(m, d) as viewer:
+	for x in range(1000):
+		dm_control.mujoco.mj_step(m, d)
+		viewer.sync()
+		time.sleep(1/100)
+	viewer.close()
