93cf56239a0c1c0ee7866ee02ba616215a951332
GarrettWeil <garrettweil2024@u.northwestern.edu>
2024-03-15 20:47:44 -0600

    mutation across generations is working!

--- simulate.py
+++ simulate.py
@@ -64,77 +64,113 @@ def add_limb(model, parent_body, parent_geom, face, thickness, length, recursive
 	return limb, geom
 
 
-parent_xml = "blank_slate.xml"
+mjcf_model = mjcf.from_path("blank_slate.xml")
 
-#model generation stage
-# Reference: https://github.com/google-deepmind/dm_control/blob/main/dm_control/mjcf/README.md
-
-mjcf_model = mjcf.from_path(parent_xml)
-
-#GENOTYPE ENCODING
-body_height = random.uniform(0.2, 3)
-body_length = random.uniform(0.2, 3)
-body_width = random.uniform(0.2, 3)
+#INITIAL GENOTYPE ENCODING
+body_height = random.uniform(0.5, 3)
+body_length = random.uniform(0.5, 3)
+body_width = random.uniform(0.5, 3)
+chance_to_skip_face = 0.05
+chance_to_recurse_more = 0.1
+chance_to_recurse_less = 0.1
 
 
-# one body
-core_body = mjcf_model.worldbody.add('body', name='core_body', pos=[0, 0, body_height])
-core_geom = core_body.add('geom', name='core_geom', type='box', size=f'{body_width} {body_length} {body_height}', rgba='0 .9 0 1')
-core_body.add('joint', name='first', type='free')
+recursion_counts = []
+leg_thicknesses = []
+leg_lengths = []
+face_flags = []
+num_generations = 5
 
-# four, three-segmented legs for each side
-for face in ['front', 'right', 'left', 'back']:
-	chance_to_skip_face = 0.05
+for generation in range(num_generations):
 
-	if random.random() < chance_to_skip_face:
-		continue
+	# indirect encoding is saved, so we can just rebuild from a blank slate each time
+	mjcf_model = mjcf.from_path("blank_slate.xml")
 
-	recursion_count = random.randrange(0, 4) #random recursions
-	leg_thickness = random.uniform(0.2, 0.7) #random leg thickness
-	leg_length = random.uniform(0.2, 5) #random leg length
+	# MUTATION
 
-
-	add_limb(mjcf_model, core_body, core_geom, face, leg_thickness, leg_length, recursion_count)
-
-# save generated file
-mjcf.export_with_assets(mjcf_model, ".", out_file_name="model.xml")
-
-#simulation and rendering stage
-DISPLAY = True
-
-
-m = mujoco.MjModel.from_xml_path("model.xml")
-d = mujoco.MjData(m)
-
-if DISPLAY:
-	viewer = mujoco_viewer.MujocoViewer(m, d)
-	#viewer.cam.azimuth = 20.0 #left right tilt
-	viewer.cam.distance = 20.0 #dist to origin
-	viewer.cam.elevation = -10 #up down tilt
-	#viewer.cam.lookat = np.array([1, 0, 1]) #forward, right, up
-
-# kp = 100
-# kv = 10
-# for actuator in range(len(d.ctrl)):
-# 	m.actuator_gainprm[actuator, 0] = 0
-# 	m.actuator_gainprm[actuator, 1] = -kp
-# 	m.actuator_gainprm[actuator, 2] = -kv
+	body_height = max(1, body_height + random.uniform(-0.2, 0.2))
+	body_length = max(1, body_length + random.uniform(-0.2, 0.2))
+	body_width = max(1, body_width + random.uniform(-0.2, 0.2))
 	
 	
 
-for i in range(4000):
-		if DISPLAY is False:
-				mujoco.mj_step(m, d)
 
-		elif DISPLAY is True and viewer.is_alive:
-				
-				# d.ctrl[1] = np.pi
+	# one body
+	core_body = mjcf_model.worldbody.add('body', name='core_body', pos=[0, 0, body_height])
+	core_geom = core_body.add('geom', name='core_geom', type='box', size=f'{body_width} {body_length} {body_height}', rgba='0 .9 0 1')
+	core_body.add('joint', name='first', type='free')
 
-				mujoco.mj_step(m, d)
-				viewer.render()
+	# four, three-segmented legs for each side
+	for index, face in enumerate(['front', 'right', 'left', 'back']):
+			if generation == 0:
+				recursion_counts.append(random.randint(0, 4))
+				leg_thicknesses.append(random.uniform(0.2, 0.5)) #random leg thickness
+				leg_lengths.append(random.uniform(0.2, 5)) #random leg length
 
-		else:
-				break
+				if random.random() < chance_to_skip_face:
+					face_flags.append(True)
+					continue
+				else:
+					face_flags.append(False)
+
+			elif face_flags[index]:
+				continue
+			
+			if random.random() < chance_to_recurse_more:
+				recursion_counts[index] = min(4, 1 + recursion_counts[index])
+			elif random.random() < chance_to_recurse_less:
+				recursion_counts[index] += 1
+
+			leg_thicknesses[index] = max(0.2, leg_thicknesses[index] + random.uniform(-0.5, 0.5))
+			leg_lengths[index] = max(0.2, leg_lengths[index] + random.uniform(-0.5, 0.5))
+	
+			add_limb(mjcf_model, core_body, core_geom, face, leg_thicknesses[index], leg_lengths[index], recursion_counts[index])
+
+	# save generated file
+	mjcf.export_with_assets(mjcf_model, ".", out_file_name=f"child_{generation}.xml")
+	print(recursion_counts)
+
+
+
+
+
+
+	#simulation and rendering stage
+	DISPLAY = True
+
+
+	m = mujoco.MjModel.from_xml_path(f"child_{generation}.xml")
+	d = mujoco.MjData(m)
+
+	if DISPLAY:
+		viewer = mujoco_viewer.MujocoViewer(m, d)
+		viewer.cam.azimuth = 20 #left right tilt
+		viewer.cam.distance = 30.0 #dist to origin
+		viewer.cam.elevation = -40 #up down tilt
+		#viewer.cam.lookat = np.array([1, 0, 1]) #forward, right, up
+
+	# kp = 100
+	# kv = 10
+	# for actuator in range(len(d.ctrl)):
+	# 	m.actuator_gainprm[actuator, 0] = 0
+	# 	m.actuator_gainprm[actuator, 1] = -kp
+	# 	m.actuator_gainprm[actuator, 2] = -kv
 		
-if DISPLAY:
-	viewer.close()
+		
+
+	for i in range(400):
+			if DISPLAY is False:
+					mujoco.mj_step(m, d)
+
+			elif DISPLAY is True and viewer.is_alive:
+					
+					# d.ctrl[1] = np.pi
+
+					mujoco.mj_step(m, d)
+					viewer.render()
+
+			else:
+					break
+			
+	if DISPLAY:
+		viewer.close()
